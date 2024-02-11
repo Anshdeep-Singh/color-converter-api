9aa72c444ddc648e62e451e2a742533e77e54dea
Anshdeep Singh <69358318+Anshdeep-Singh@users.noreply.github.com>
2024-02-11 02:59:18 -0800

    updated initial text

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,12 @@ app.get("/",(ctx)=>{
 5. hex
 
 To use the API, use the following URL format:
-https://color-converter-api.anshdeepsaini.workers.dev/colorformat/colorname`
+https://color-converter-api.anshdeepsaini.workers.dev/colorformat/colorname 
+
+OR: 
+
+https://colorconvert.anshdeepsingh.com/colorformat/colorname
+`
 	);
 })
 
